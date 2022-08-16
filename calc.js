6567d55cd6f33524bf97be0220f664c8f8e65750
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2022-08-16 12:19:41 +0900

    Add alphanumeric hash values up to 3 digits

---
+++ calc.js
@@ -0,0 +1,37 @@
+var crypto = require('crypto');
+var fs = require('fs');
+var length = 3;
+function product(iterables, repeat) {
+    var argv = Array.prototype.slice.call(arguments), argc = argv.length;
+    if (argc === 2 && !isNaN(argv[argc - 1])) {
+        var copies = [];
+      for (var i = 0; i < argv[argc - 1]; i++) {
+          copies.push(argv[0].slice()); // Clone
+      }
+      argv = copies;
+    }
+    return argv.reduce(function tl(accumulator, value) {
+      var tmp = [];
+      accumulator.forEach(function(a0) {
+        value.forEach(function(a1) {
+          tmp.push(a0.concat(a1));
+        });
+      });
+      return tmp;
+    }, [[]]);
+  }
+var name = product('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.match(/.{1}/g), length).filter(word => word.join("").toUpperCase() === "AUX" || word.join("").toUpperCase() === "CON" || word.join("").toUpperCase() === "NUL" || word.join("").toUpperCase() === "PRN" );
+console.log(name)
+/*
+fs.mkdir('encrypt/' + length, (err) => {
+    for (let i = 0; i < name.length; i++) {
+        var hash = crypto.createHash('sha256').update(name[i].join("")).digest('hex');
+        fs.writeFileSync('encrypt/' + length + "/" + name[i].join(""), hash);
+        fs.writeFileSync('decrypt/' + hash, name[i].join(""));
+        console.log(name[i].join("") + ":" + hash);
+    }
+});
+*/
+for (let i = 0; i < name.length; i++) {
+fs.unlinkSync("encrypt/3/"+name[i].join(""));
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# hash-db/sha256
+**This repository serves as a database and API for storing strings hashed by sha-256.**
+The directory structure is as follows
+
+    encrypt/<number of characters in string to be hashed>/<string to be hashed>
+
+    decrypt/<string to be hashed>/<string to be hashed>
+
+## hou to use?
+With this structure, you can get the data you want with the following command
+For example, if you want to know the hash value of the string "AAA"
+
+    curl https://raw.githubusercontent.com/hash-db/sha256/main/encrypt/3/AAA
+
+to get the hash value of the string AAA.
+Conversely, if you want to get the hash value of "AAA" from "cb1ad2119d8fafb69566510ee712661f9f14b83385006ef92aec47f523a38358
+
+    curl https://raw.githubusercontent.com/hash-db/sha256/main/decrypt/cb1ad2119d8fafb69566510ee712661f9f14b83385006ef92aec47f523a38358
+
+to obtain it.
+
+## please add more hash plz!
+This repository still supports only 4-digit alphanumeric characters.
+If you are able to calculate more than 5 alphanumeric digits, please make a Pull Request.
+This repository is limited by one person's computing resources. Your help is essential.
