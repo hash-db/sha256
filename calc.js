beaedf3b1f3561bd3e6006dcb229b7b619f5a451
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-07-19 11:43:20 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -109,20 +109,7 @@ if (cluster.isPrimary) {
             }, 3000);
         } else {
             if (message == "con" || message == "prn" || message == "aux" || message == "nul" || message == "com1" || message == "com2" || message == "com3" || message == "com4" || message == "com5" || message == "com6" || message == "com7" || message == "com8" || message == "com9" || message == "lpt1" || message == "lpt2" || message == "lpt3" || message == "lpt4" || message == "lpt5" || message == "lpt6" || message == "lpt7" || message == "lpt8" || message == "lpt9") return process.send("end");          
-            console.log("check existing")
-            const options = {
-                hostname: 'raw.githubusercontent.com',
-                port: 443,
-                path: `/hash-db/sha256/main/encrypt/${message.length}/${message}`,
-                method: 'GET'
-            }
-            https.request(options, res => {
-                console.log("check finish")
-                if (res.statusCode == 200) {
-                    console.log("existed")
-                    return process.send("end");
-                } else {        
-                    console.log("not found")
+            
             let hash = crypto.createHash('sha256').update(message).digest('hex');
                     if (!fs.existsSync(`./encrypt/`)){
                         fs.mkdirSync(`./encrypt/`);
@@ -140,8 +127,7 @@ if (cluster.isPrimary) {
                     fs.writeFileSync('decrypt/' + hash, message);
                     console.log(message + ":" + hash);
                     process.send({ type: "end", value: [message, hash] });
-                }
-            });
+
         }
     });
 }
